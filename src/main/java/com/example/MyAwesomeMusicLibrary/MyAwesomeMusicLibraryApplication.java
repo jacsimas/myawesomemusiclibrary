a3e694c249ece603ee5114bf61e871be9c37ce68
jacsimas <jacsimas@gmail.com>
2026-07-03 13:22:52 +0200

    first commit

---
+++ src/main/java/com/example/MyAwesomeMusicLibrary/MyAwesomeMusicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyAwesomeMusicLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwesomeMusicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwesomeMusicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyAwesomeMusicLibrary/MyAwesomeMusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyAwesomeMusicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAwesomeMusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
